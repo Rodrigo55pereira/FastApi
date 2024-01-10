0d325d341aadbca5d7313284df000a52bc2094a3
Rpereira <rpereira@pop-os.localdomain>
2024-01-10 09:43:11 -0300

    Criação do projeto app-jogadores responsável pelo front e comunicação com a API

---
+++ app-jogadores/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="mt-3 justify-content-center align-items-center mx-auto"
+        style={{ "width": "60vw", "backgroundColor": "#ffffff" }}
+      >
+        <h2 className="text-center text-white bg-success card mb-1">Gerenciamento de Jogadores</h2>
+        <h6 className='card text-center text-white bg-success mb-1'>Informações de Jogador</h6>
+      </div>
+    </div>
+  );
+}
+
+export default App;

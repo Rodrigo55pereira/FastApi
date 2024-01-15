888af12c5bced28c29cafd1cc5b366301fd6de19
Rpereira <rpereira@pop-os.localdomain>
2024-01-15 14:50:37 -0300

    Atualização para criação de modal de confirmação

--- app-jogadores/src/components/Jogador.js
+++ app-jogadores/src/components/Jogador.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function Jogador(props) {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedJogadorId, setSelectedJogadorId] = useState(null);
 
     // Função para deletar jogador
     const excluirJogador = (jogadorId) => {
@@ -9,9 +11,16 @@ function Jogador(props) {
             .then(
                 resposta => {
                     alert("Jogador removido com sucesso" + resposta.data)
+                    setShowModal(false);
                 }
             )
     }
+
+    const confirmarExclusao = (jogadorId) => {
+        setSelectedJogadorId(jogadorId);
+        setShowModal(true);
+    }
+
     const editaJogador = (jogador) => {
         props.setJogadorId(jogador.id)
         props.setJogadorNome(jogador.nome)
@@ -32,12 +41,37 @@ function Jogador(props) {
                     <span className="badge rounded-pill bg-info">Editar</span>
                 </button>
                 <button
-                    onClick={() => excluirJogador(props.jogador.id)}
+                    onClick={() => confirmarExclusao(props.jogador.id)}
                     className="btn btn-sm"
                 >
                     <span className="badge rounded-pill bg-danger">X</span>
                 </button>
             </p>
+
+            {showModal && (
+                <div className="modal" style={{ display: 'block' }}>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Confirmação</h5>
+                                <button type="button" className="btn-close" onClick={() => setShowModal(false)}></button>
+                            </div>
+                            <div className="modal-body">
+                                Você tem certeza que deseja excluir o jogador?
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" onClick={() => setShowModal(false)}>
+                                    Cancelar
+                                </button>
+                                <button type="button" className="btn btn-danger" onClick={() => excluirJogador(selectedJogadorId)}>
+                                    Sim, excluir
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
         </div>
     );
 }
